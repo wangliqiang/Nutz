7ec8cbf7a4597c7b8c003a7f82659c344e1ced01
王立强 <wangliqiangorz@163.com>
2017-01-06 14:46:44 +0800

    update README.md

--- app/src/main/java/com/app/nuts/App.java
+++ app/src/main/java/com/app/nuts/App.java
@@ -14,19 +14,13 @@ import timber.log.Timber;
  */
 
 public class App extends Application {
+
     private static Context mContext;
+
     @Override
     public void onCreate() {
         super.onCreate();
         mContext = this;
-        /**
-         * 初始化Timber
-         */
-        if (BuildConfig.DEBUG) {
-            Timber.plant(new Timber.DebugTree());
-        } else {
-            Timber.plant(new CrashReportingTree());
-        }
         if (LeakCanary.isInAnalyzerProcess(this)) {
             return;
         }
@@ -34,52 +28,14 @@ public class App extends Application {
          * 初始化LeakCanary
          */
         LeakCanary.install(this);
-        /**
-         * 初始化OkGo
-         *//*
-        OkGo.init(this);
-        try {
-            //全局参数设置
-            OkGo.getInstance()
-                    //打开调试开关
-                    .debug("OkGo", Level.SEVERE, true)
-                    //设置超时时间，默认60秒
-                    .setConnectTimeout(OkGo.DEFAULT_MILLISECONDS)
-                    .setReadTimeOut(OkGo.DEFAULT_MILLISECONDS)
-                    .setWriteTimeOut(OkGo.DEFAULT_MILLISECONDS)
-                    //设置全局缓存，默认无缓存
-                    .setCacheMode(CacheMode.NO_CACHE)
-                    //设置全局缓存时间，默认永不过期
-                    .setCacheTime(CacheEntity.CACHE_NEVER_EXPIRE);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
     }
 
     /**
      * 设置全局的Context
+     *
      * @return
      */
-    public static Context getmContext(){
+    public static Context getmContext() {
         return mContext;
-    }
-
-    /** A tree which logs important information for crash reporting. */
-    private static class CrashReportingTree extends Timber.Tree {
-        @Override protected void log(int priority, String tag, String message, Throwable t) {
-            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
-                return;
-            }
-
-            FakeCrashLibrary.log(priority, tag, message);
-
-            if (t != null) {
-                if (priority == Log.ERROR) {
-                    FakeCrashLibrary.logError(t);
-                } else if (priority == Log.WARN) {
-                    FakeCrashLibrary.logWarning(t);
-                }
-            }
-        }
     }
 }
