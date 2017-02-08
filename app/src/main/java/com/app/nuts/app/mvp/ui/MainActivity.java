113167749317ba49d0d3dbee3eac546fa36ea2d8
王立强 <wangliqiangorz@163.com>
2017-02-08 14:59:30 +0800

    add lottie-android and AppIntro

--- app/src/main/java/com/app/nuts/app/mvp/ui/MainActivity.java
+++ app/src/main/java/com/app/nuts/app/mvp/ui/MainActivity.java
@@ -3,8 +3,10 @@ package com.app.nuts.app.mvp.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Button;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.app.nuts.R;
 import com.app.nuts.app.common.AppComponent;
 import com.app.nuts.app.common.BaseActivity;
@@ -19,6 +21,10 @@ public class MainActivity extends BaseActivity {
     Button movieBtn;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.animation_view)
+    LottieAnimationView animationView;
+    @BindView(R.id.intro_btn)
+    Button introBtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,8 +41,15 @@ public class MainActivity extends BaseActivity {
 
     }
 
-    @OnClick(R.id.movie_btn)
-    public void onClick() {
-        startActivity(new Intent(this, MovieActivity.class));
+    @OnClick({R.id.movie_btn, R.id.intro_btn})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.movie_btn:
+                startActivity(new Intent(this, MovieActivity.class));
+                break;
+            case R.id.intro_btn:
+                startActivity(new Intent(this, AppIntroActivity.class));
+                break;
+        }
     }
 }
